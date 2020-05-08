1e85b3ab5a20af0d8aed8cfb45753ed64daba5e1
Konstantin Bondarenko <kosntantinbo92@gmail.com>
2020-05-08 18:22:54 +0300

    Updates

---
+++ precache-manifest.52f78e8a052e6f6555815d89c1924324.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "164ecf687fbb2b660bb1",
+    "url": "/react-hooks-practice/static/js/runtime~main.b93f1e9f.js"
+  },
+  {
+    "revision": "3ee42752b9c8c1e4ed76",
+    "url": "/react-hooks-practice/static/js/main.149bc736.chunk.js"
+  },
+  {
+    "revision": "27d6a87cd418b1425b69",
+    "url": "/react-hooks-practice/static/js/2.d06e9420.chunk.js"
+  },
+  {
+    "revision": "3ee42752b9c8c1e4ed76",
+    "url": "/react-hooks-practice/static/css/main.cf73be18.chunk.css"
+  },
+  {
+    "revision": "27d6a87cd418b1425b69",
+    "url": "/react-hooks-practice/static/css/2.94be9347.chunk.css"
+  },
+  {
+    "revision": "51f416970fc5256ccb15e6e74248831b",
+    "url": "/react-hooks-practice/index.html"
+  }
+];
